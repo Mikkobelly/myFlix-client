cef4d3bfe563b15976ef3266edbec1fc1707bc8c
Mikkobelly <mikiakuta0529@gmail.com>
2023-02-05 01:08:55 +0000

    Updated to load movies from myFlix API and added prop-types constraint

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,31 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "../movie-card/movie-card";
 import MovieView from "../movie-view/movie-view";
 
 const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Silence Of Lambs",
-            description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
-            genre: "Thriller",
-            director: "Jonathan Demme",
-            image: "https://m.media-amazon.com/images/I/51SHYSFNP2L._AC_.jpg"
-        },
-        {
-            id: 2,
-            title: "The Dark Knight",
-            description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-            genre: "Crime",
-            director: "Christopher Nolan",
-            image: "https://m.media-amazon.com/images/I/516JxYYnE8L._AC_.jpg"
-        },
-        {
-            id: 3,
-            title: "Inception",
-            description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.",
-            genre: "Action",
-            director: "Christopher Nolan",
-            image: "https://m.media-amazon.com/images/I/51C6DPWQwgL._AC_.jpg"
-        }
-    ])
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://myflix-by-mikkobelly.herokuapp.com/movies")
+            .then(res => res.json())
+            .then((movies) => {
+                const moviesFromApi = movies.map((movie) => {
+                    return {
+                        id: movie._id,
+                        title: movie.Title,
+                        description: movie.Description,
+                        genre: movie.Genre.Name,
+                        director: movie.Director.Name,
+                        image: movie.ImagePath
+                    };
+                });
+
+                setMovies(moviesFromApi);
+            })
+            .catch(err => console.log(err));
+    }, []);
 
     if (selectedMovie) {
         return <MovieView movie={selectedMovie} onBackClick={() => { return setSelectedMovie(null); }} />
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MovieCard = ({ movie, onMovieClick }) => {
     return <div onClick={() => onMovieClick(movie)}>{movie.title}</div>
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
 
 export default MovieCard;
